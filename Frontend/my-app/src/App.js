d4184360537985aaa47fe801819bbd30b850ec7d
mohitbaviskar <mohitbaviskar360@gmail.com>
2023-09-03 17:31:42 +0530

    basic routes

--- Frontend/my-app/src/App.js
+++ Frontend/my-app/src/App.js
@@ -2,12 +2,23 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import UserRegistration from './components/UserRegistration';
 import UserLogin from './components/UseLogin';
+import Home from "./components/Home";
+import Layout from './components/Layout';
+import NoPage from './components/Nopage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-     {// <UserRegistration/>
-    }
-      <UserLogin/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="login" element={<UserLogin />} />
+          <Route path="register" element={<UserRegistration />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
